e9d86225aa83efeb0e8566a081f410fe93eee7b6
Marek Kundla <Marek.Kundla@gmail.com>
2023-09-07 11:00:36 +0200

    hangman word generating and guessing

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,10 @@ function App() {
                 Lose Win
             </div>
             <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-            <HangmanWord />
+            <HangmanWord
+                guessedLetters={guessedLetters}
+                wordToGuess={wordToGuess}
+            />
             <div style={{ alignSelf: 'stretch' }}>
                 <Keyboard />
             </div>
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+type HangmanWordProps = {
+    guessedLetters: string[]
+    wordToGuess: string
+}
 
-function HangmanWord() {
-    const word = 'TEST'
-    const guessedLetters = ['e']
-
+function HangmanWord({ guessedLetters, wordToGuess }: HangmanWordProps) {
     return (
         <div
             style={{
@@ -15,7 +15,7 @@ function HangmanWord() {
                 fontFamily: 'monospace',
             }}
         >
-            {word.split('').map((letter, index) => (
+            {wordToGuess.split('').map((letter, index) => (
                 <span style={{ borderBottom: '.1em solid black' }} key={index}>
                     <span
                         style={{
